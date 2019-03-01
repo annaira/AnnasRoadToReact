92d66a6a79664a7a4ec3efa0b023f4e528fc9c00
Anna Ira Hurnaus <anna.ira.hurnaus@we-wash.com>
2019-03-01 14:24:07 +0100

    formatting

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -2,37 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 
 const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    }
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
 ]
 
 function isSearched(searchTerm) {
   return function (item) {
-  return item.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+    return item.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
   }
-  }
+}
 class App extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-        list,
-        searchTerm: '',
+      list,
+      searchTerm: '',
     };
 
     this.onSearchChange = this.onSearchChange.bind(this);
@@ -40,10 +40,10 @@ class App extends Component {
   }
 
   onSearchChange(event) {
-    this.setState({ searchTerm:  event.target.value });
+    this.setState({ searchTerm: event.target.value });
   }
 
-  onDismiss(id){
+  onDismiss(id) {
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
     this.setState({ list: updatedList });
