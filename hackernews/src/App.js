9d75c6523969a435d1ee7725cee1da1dbc4ad662
Anna Ira Hurnaus <anna.ira.hurnaus@we-wash.com>
2019-03-01 11:41:28 +0100

    ES6 Arrow Functions

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -22,20 +22,18 @@ const list = [
 
 class App extends Component {
   render() {
-    const helloWorld = 'Welcome to the Road to learn React';
     return (
       <div className="App">
-          {list.map(function (item) {
-            return (<div key={item.objectID}>
-                      <span>
-                        <a href={item.url}>{item.title}</a>
-                      </span>
-                      <span>{item.author}</span>
-                      <span>{item.num_comments}</span>
-                      <span>{item.points}</span>
-                    </div>
-              );
-          })}
+          {list.map(item =>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          )}
       </div>
     );
   }
