7c3bf486e62286063bea923ad13845893164b4a2
Anna Ira Hurnaus <anna.ira.hurnaus@we-wash.com>
2019-03-29 10:55:37 +0100

    Loading ...

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
       results: null,
       searchKey: "",
       searchTerm: DEFAULT_QUERY,
-      error: null
+      error: null,
+      isLoading: false,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -48,11 +49,13 @@ class App extends Component {
       results: {
         ...results,
         [searchKey]: { hits: updatedHits, page }
-      }
+      },
+      isLoading: false
     });
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
+    this.setState({ isLoading: true });
     axios(
       `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
     )
@@ -97,7 +100,7 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, results, searchKey, error } = this.state;
+    const { searchTerm, results, searchKey, error, isLoading } = this.state;
     const page =
       (results && results[searchKey] && results[searchKey].page) || 0;
     const list =
@@ -106,6 +109,10 @@ class App extends Component {
     return (
       <div className="page">
         <div className="interactions">
+          { isLoading
+            ? <Loading/>
+            : <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>More</Button>
+          }
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
@@ -216,6 +223,9 @@ Button.propTypes = {
 Button.defaultProps = {
   className: '',
 };
+
+const Loading = () =>
+  <div>Loading ...</div>
 
 export default App;
 
